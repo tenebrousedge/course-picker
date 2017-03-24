fabfa5fcd55f4cf78aa27c110d4abce849beb7a1
Patrick Leahy <tenebrousedge@gmail.com>
2017-03-24 11:43:51 -0700

    Add images, markup, and most of the rest of the javascript
    Because four lines of a javascript function is TOO MANY LINES O__O

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,52 @@
+var es6Check = function() {
+	try {
+		var testVariable = 'test';
+		// convert to boolean and return
+		return !!`I may or may not be able to ${testVariable} this`;
+	} catch (e) {return false;}
+};
+
 $(document).ready(function() {
+	var slide = function(elem, direction) {
+		elem.hide('slide', {direction: direction}, 300);
+	};
+
 	var nextQuestion = function() {
+		slide($('+ .question', this), 'left');
+	};
 
-	}
 	var previousQuestion = function() {
+		slide($('+ .question', this), 'left');
+	};
 
-	}
-	var getAnswersFromForm = function(e) {
+	var getAnswersFromForm = function() {
+		return $('form input').map(function(idx, elem) {
+			//this will have to be more complex
+			return parseInt(elem.val());
+		});
+	};
 
-	}
+	//expected structure [{ruby:int},{php:int},{android:int}
 	var determineTrack = function(answers) {
+		Object.keys(answers).reduce(function(a, b){ return answers[a] > answers[b] ? a : b });
+	};
 
-	}
+	var getResult = function() {
+		return determineTrack(getAnswersFromForm());
+	};
+
+	var showResult = function(result) {
+		if (es6Check()) {
+			$(`#${result}`).show();
+		} else {
+			$('#' + result).show();
+		}
+	};
+
+	$('button#next').click(nextQuestion);
+	$('button#prev').click(previousQuestion);
+	$('button#submit').click(function() {
+		showResult(getResult());
+		return false;
+	});
 });
